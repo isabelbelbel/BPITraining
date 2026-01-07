582f5fec63e3b3f9b132cbd40996ed8cd226f6c6
isabelbelbel <isabelcmorales09@gmail.com>
2026-01-07 17:05:57 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package M4_Activity1;
+
+public class Main {
+
+	 public static void main(String[] args) {
+		 	
+		 BankAccount bnkAcc = new BankAccount();
+		 
+		 System.out.println("=== Bank Account Name System ===\n");
+		 bnkAcc.testCase("ACC-001");
+		 bnkAcc.testCase("ACC-999");
+		 System.out.println("=== Program Completeed Successfully! ===");
+	 }
+}
+
+
+
+
